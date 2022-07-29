5471bba0254f056d3c894253ac0bc7f2c57cf8d3
lim <sherwinlim8899@gmail.com>
2022-07-29 12:55:16 +0800

    pdf generator

--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,18 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+Imports Newtonsoft.Json
+Public Class Form1
     Private Sub generatebtn_Click(sender As Object, e As EventArgs) Handles generatebtn.Click
+        Dim createpdf As Document = New Document()
+        PdfWriter.GetInstance(createpdf, New FileStream("Lim_Sherwin.pdf", FileMode.Create))
+        createpdf.Open()
+        createpdf.Add(New Paragraph("text"))
+        createpdf.Close()
+        MsgBox("Created Successfully")
+
 
     End Sub
+
+
 End Class
