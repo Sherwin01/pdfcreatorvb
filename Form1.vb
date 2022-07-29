82fb804d32e9d31de7c1b83f784acf9ad28e8a25
lim <sherwinlim8899@gmail.com>
2022-07-29 12:57:39 +0800

    create public class for json

--- Form1.vb
+++ Form1.vb
@@ -13,6 +13,16 @@ Public Class Form1
 
 
     End Sub
-
+    Public Class info
+        Public FullName As String
+        Public Age As String
+        Public Birthday As String
+        Public Address As String
+        Public ContactNumber As String
+        Public Email As String
+        Public objective As String
+        Public education As String
+        Public skills As String
+    End Class
 
 End Class
