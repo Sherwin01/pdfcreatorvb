c6469d9ee0b0ee85212616f454e1f86747117693
lim <sherwinlim8899@gmail.com>
2022-07-29 13:10:01 +0800

    var

--- Form1.vb
+++ Form1.vb
@@ -3,11 +3,16 @@ Imports iTextSharp.text
 Imports iTextSharp.text.pdf
 Imports Newtonsoft.Json
 Public Class Form1
+    Dim jsonreader As String = File.ReadAllText("test.json")
+    Dim jsoninfo As info = JsonConvert.DeserializeObject(Of info)(jsonreader)
+
     Private Sub generatebtn_Click(sender As Object, e As EventArgs) Handles generatebtn.Click
         Dim createpdf As Document = New Document()
         PdfWriter.GetInstance(createpdf, New FileStream("Lim_Sherwin.pdf", FileMode.Create))
         createpdf.Open()
-        createpdf.Add(New Paragraph("text"))
+        Dim fn As Paragraph = New Paragraph(jsoninfo.FullName)
+        fn.Font.Size = 16
+        createpdf.Add(fn)
         createpdf.Close()
         MsgBox("Created Successfully")
 
